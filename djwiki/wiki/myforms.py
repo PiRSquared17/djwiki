79aa1941a1594ebbaac2fe5620ce55eb3ccd466b
alert.main <alert.main@39691643-ab46-0410-bf78-7956d45dac46>
2008-02-21 22:25:47 +0000

    для отображения содержимого страницы написан новый widget - BigTextWidget в форму добавлено новое поле - markupType

--- djwiki/wiki/myforms.py
+++ djwiki/wiki/myforms.py
@@ -1,5 +1,5 @@
 from djwiki.wiki.models import *
-from django.newforms import ModelForm, TextInput, IntegerField, CharField, ModelChoiceField, Select
+from django.newforms import *
 from django.utils.encoding import StrAndUnicode, force_unicode
 from django.utils.safestring import mark_safe
 from django.newforms.util import flatatt
@@ -14,10 +14,19 @@ class DisabledSelectWidget(Select):
         kwargs.setdefault('attrs',{}).update({'disabled': 'True'})
         super(DisabledSelectWidget, self).__init__(*args, **kwargs)
 
+class BigTextWidget(Textarea):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('attrs',{}).update({'cols': '160', 'rows': '40'})
+        super(BigTextWidget, self).__init__(*args, **kwargs)
+
 
 class WikiEditForm(ModelForm):
   revision = IntegerField(widget=DisabledTextWidget(), required=False)
   title = ModelChoiceField(queryset=WikiPageTitle.objects.all(), widget=DisabledSelectWidget(), required=False)
+  content = CharField(widget=BigTextWidget())
+  markup_types = (('markdown', 'markdown'), ('wikimedia', 'wikimedia'))
+  markupType = ChoiceField(choices=markup_types)
+
   class Meta:
     model = WikiPageContent
 
