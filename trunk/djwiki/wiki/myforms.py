efd53f9629b674d851511c6770c951274df79dcd
alert.main <alert.main@39691643-ab46-0410-bf78-7956d45dac46>
2008-02-23 12:20:01 +0000

    Все тут упростил, заменил атрибут disabled на readonly, т.к. при использовании disabled данные с input'a не помещаются в POST запрос

--- trunk/djwiki/wiki/myforms.py
+++ trunk/djwiki/wiki/myforms.py
@@ -1,32 +1,16 @@
 from djwiki.wiki.models import *
 from django.newforms import *
-from django.utils.encoding import StrAndUnicode, force_unicode
-from django.utils.safestring import mark_safe
-from django.newforms.util import flatatt
 
-class DisabledTextWidget(TextInput):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('attrs',{}).update({'disabled': 'True'})
-        super(DisabledTextWidget, self).__init__(*args, **kwargs)
-
-class DisabledSelectWidget(Select):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('attrs',{}).update({'disabled': 'True'})
-        super(DisabledSelectWidget, self).__init__(*args, **kwargs)
-
-class BigTextWidget(Textarea):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('attrs',{}).update({'cols': '160', 'rows': '40'})
-        super(BigTextWidget, self).__init__(*args, **kwargs)
-
+class ReadOnlyText(TextInput):
+  def __init__(self, *args, **kwargs):
+    kwargs.setdefault('attrs',{}).update({'readonly': 'readonly'})
+    super(ReadOnlyText, self).__init__(*args, **kwargs)
 
 class WikiEditForm(ModelForm):
-  revision = IntegerField(widget=DisabledTextWidget(), required=False)
-  title = ModelChoiceField(queryset=WikiPageTitle.objects.all(), widget=DisabledSelectWidget(), required=False)
-  content = CharField(widget=BigTextWidget())
-  markup_types = (('markdown', 'markdown'), ('wikimedia', 'wikimedia'))
-  markupType = ChoiceField(choices=markup_types)
+  revision = IntegerField(widget=ReadOnlyText())
+  title = CharField(widget=ReadOnlyText())
+  content = CharField(widget=Textarea(attrs={'rows':40, 'cols':160}))
+  markupType = ChoiceField(choices=(('markdown', 'markdown'), ('wikimedia', 'wikimedia')))
 
   class Meta:
     model = WikiPageContent
-
