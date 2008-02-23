1f30817ac06a6c312fcf1e39a7261ee600e54947
alskvorcov <alskvorcov@39691643-ab46-0410-bf78-7956d45dac46>
2008-02-23 18:03:31 +0000

    1. Main navigation menu 2. Page list (base functionality) view and spec. functions should be discussed SAN

--- trunk/djwiki/urls.py
+++ trunk/djwiki/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
      (r'^admin/', include('django.contrib.admin.urls')),
      (r'^wiki/', include('djwiki.wiki.urls')),
+     (r'^list/', 'djwiki.wiki.views.pages_list'),
 )
