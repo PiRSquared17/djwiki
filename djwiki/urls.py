330c31139856e45f368cadc74fc88d2ac60d8eee
alskvorcov <alskvorcov@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-03 21:02:25 +0000

    Проблемы при добавлении комментов

--- djwiki/urls.py
+++ djwiki/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
 from djwiki import settings
+from django.contrib.comments.models import FreeComment
+
 
 urlpatterns = patterns('',
      (r'^admin/', include('django.contrib.admin.urls')),
      (r'^wiki/', include('djwiki.wiki.urls')),
+     (r'^comments/', include('django.contrib.comments.urls.comments')),
      (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
