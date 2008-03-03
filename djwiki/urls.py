116d2d1d8af58083575a2e68c454bc098e055cdb
alskvorcov <alskvorcov@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-03 22:21:10 +0000

    Добавлены FreeComments добавление но не просмотр удалось избежать правки модуля comments - шаблоны согласились жить с шаблонами wiki

--- djwiki/urls.py
+++ djwiki/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 from djwiki import settings
 from django.contrib.comments.models import FreeComment
 
-
 urlpatterns = patterns('',
      (r'^admin/', include('django.contrib.admin.urls')),
      (r'^wiki/', include('djwiki.wiki.urls')),
+     (r'^comments/postfree/', 'views.my_post_free_comment'),
      (r'^comments/', include('django.contrib.comments.urls.comments')),
      (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
