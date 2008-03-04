f52ca21e46ae7d855f087bfcadf4964b20395ebd
alert.main <alert.main@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-04 15:57:21 +0000

    url на загруженный файлы перенесен в /wiki/urls.py

--- djwiki/urls.py
+++ djwiki/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from djwiki import settings
 from django.contrib.comments.models import FreeComment
 
 urlpatterns = patterns('',
@@ -7,5 +6,4 @@ urlpatterns = patterns('',
      (r'^wiki/', include('djwiki.wiki.urls')),
      (r'^comments/postfree/', 'views.my_post_free_comment'),
      (r'^comments/', include('django.contrib.comments.urls.comments')),
-     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
