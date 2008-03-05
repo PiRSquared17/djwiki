3ec727265a5c73ce156a5f28c32471513bb4e546
alert.main <alert.main@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-05 21:18:17 +0000

    добавил строки wiki/pageName/rev - для просмотра списка ревизий и wiki/pageName/diff для просмотра разницы между ревизиями

--- trunk/djwiki/wiki/urls.py
+++ trunk/djwiki/wiki/urls.py
@@ -7,7 +7,7 @@ from djwiki import settings
 
 urlpatterns = patterns('django.views.generic.simple',
   (r'^list/$','direct_to_template', {'template':'wiki/pages_list.html',
-                                     'extra_context': {'title': 'Pages list',
+                                     'extra_context': {'title':'Page list', 
                                                        'list':WikiPageTitle.objects.all()}}),
   (r'^$', 'redirect_to', {'url': '/wiki/home/'}),
 )
@@ -21,7 +21,9 @@ urlpatterns += patterns('',
   (r'^(?P<page_title>[^/]+)/$', 'djwiki.wiki.views.view_page', {'rev':0,'is_head':True}),
   (r'^(?P<page_title>[^/]+)/edit/$', 'djwiki.wiki.views.edit_page'),  
   (r'^(?P<page_title>[^/]+)/create/$', 'djwiki.wiki.views.create_page'),
+  (r'^(?P<page_title>[^/]+)/rev/$', 'djwiki.wiki.views.view_revisions'),
   (r'^(?P<page_title>[^/]+)/rev/(?P<rev>\d+)/$', 'djwiki.wiki.views.view_page', {'is_head':False}),
+  (r'^(?P<page_title>[^/]+)/diff/$', 'djwiki.wiki.views.diff_page'),
   (r'^static/(?P<type>[^/]*)/(?P<page>[^/]*)/(?P<file>[^/]*)/$', 'djwiki.wiki.views.view_file'),
   (r'^dynamic/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
