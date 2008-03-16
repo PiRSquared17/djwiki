bb1e372e895689d337ed28a4aca83a3bcdf90dc5
alskvorcov <alskvorcov@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-16 16:30:12 +0000

    Добавил функции login и logout отобраение текущего пользователя под строкой меню

--- djwiki/urls.py
+++ djwiki/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 from django.contrib.comments.models import FreeComment
 
 urlpatterns = patterns('',
+     (r'^accounts/login/$', 'django.contrib.auth.views.login'),  
+     (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
      (r'^admin/', include('django.contrib.admin.urls')),
      (r'^wiki/', include('djwiki.wiki.urls')),
      (r'^comments/postfree/', 'views.my_post_free_comment'),
