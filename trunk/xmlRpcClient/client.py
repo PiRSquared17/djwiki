b7134681a8201d7dc16ea0a79d1314961332829d
alert.main <alert.main@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-18 12:53:43 +0000

    изменил примеры обращений к xml-rpc серверу

--- trunk/xmlRpcClient/client.py
+++ trunk/xmlRpcClient/client.py
@@ -1,28 +1,14 @@
 import sys
 import xmlrpclib
 
-selecttext = """SELECT * FROM wiki_wikipagetitle WHERE id = 6"""
-
-updatetext = """
-UPDATE wiki_wikipagetitle SET title = 'home' WHERE id = 1
-"""
-insertTitleText = """
-REPLACE INTO wiki_wikipagetitle VALUES(2, 'xmlrpc', 0)
-"""
-insertContentText = """
-REPLACE INTO wiki_wikipagecontent 
-VALUES(2, 2, 'This page was added via xml-rpc. Hello Again!', 'ALeRT', 0, '2008-03-05 23:30:11.500000', 'markdown', '')
-"""
-
-text = """
-INSERT INTO wiki_wikipagetitle VALUES(6, 'xmlrpc', 0);
-SELECT * FROM wiki_wikipagetitle WHERE id = 6;
-"""
-
-
 rpc_srv = xmlrpclib.ServerProxy("http://localhost:8000/xmlrpc/")
 
-rpc_srv.execsql([insertTitleText, insertContentText])
+#print rpc_srv.create_page("hello", "hello", "rpc-bot", "wikimedia", "rpc test")
+#print rpc_srv.update_page("test1", "test hello", "rpc-bot", "wikimedia", "basic test rpc")
+#print rpc_srv.get_revision("test1", 1)
+#print rpc_srv.get_category("cat1")
+print rpc_srv.add_category("cat1", "rpc3")
+
 
 
 
