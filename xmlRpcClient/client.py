7b96e17918486f94f3a9fdea17a66dfb33079860
alert.main <alert.main@39691643-ab46-0410-bf78-7956d45dac46>
2008-03-19 20:35:21 +0000

    добавил использование аутентификации

--- xmlRpcClient/client.py
+++ xmlRpcClient/client.py
@@ -1,13 +1,13 @@
 import sys
 import xmlrpclib
 
-rpc_srv = xmlrpclib.ServerProxy("http://localhost:8000/xmlrpc/")
+rpc_srv = xmlrpclib.ServerProxy("http://localhost:8000/xmlrpc/?u=admin&p=qqq")
 
 #print rpc_srv.create_page("hello", "hello", "rpc-bot", "wikimedia", "rpc test")
 #print rpc_srv.update_page("test1", "test hello", "rpc-bot", "wikimedia", "basic test rpc")
-#print rpc_srv.get_revision("test1", 1)
-#print rpc_srv.get_category("cat1")
-print rpc_srv.add_category("cat1", "rpc3")
+print rpc_srv.get_revision("home", 0)
+#print rpc_srv.get_category("Main")
+#print rpc_srv.add_category("cat1", "rpc3")
 
 
 
